174a998ffaed8e3d6108924472b169ab092bd404
RoryPowell <rpowell@student.unimelb.edu.au>
2018-03-27 18:31:00 +1100

    made next_to and two_spaces_away functions universal

--- a_massacre.py
+++ a_massacre.py
@@ -1,5 +1,7 @@
 import watchurback
 
+WHITE = 'white'
+BLACK = 'black'
 
 def on_action(action: str, board: watchurback.BlankBoard):
     # TODO
@@ -15,27 +17,48 @@ def on_action(action: str, board: watchurback.BlankBoard):
 
     print('MASSACRE still being implemented.')
 
+def is_vuln()
 
 def can_take(board: watchurback.BlankBoard, xy: tuple):
     x = xy[0]
     y = xy[1]
 
-    return next_to(board, x, y) and two_spaces_away(board, x, y)
+    return next_to(BLACK, board, x, y) and two_spaces_away(WHITE, board, x, y)
 
 
-def next_to(board: watchurback.BlankBoard, x, y):
-    for ii in board.index_black:
-        if ((x + 1 == ii[0] or x - 1 == ii[0]) and (y == ii[1])) or \
+def next_to(colour, board: watchurback.BlankBoard, x, y):
+    # returns whether a piece is next to x,y
+    if colour == BLACK:
+        for ii in board.index_black:
+            if ((x + 1 == ii[0] or x - 1 == ii[0]) and (y == ii[1])) or \
                 ((y + 1 == ii[1] or y - 1 == ii[1]) and (x == ii[0])):
-            return True
-        else:
-            return False
+                return True
+            else:
+                return False
+    elif colour == WHITE:
+        for ii in board.index_white:
+            if ((x + 1 == ii[0] or x - 1 == ii[0]) and (y == ii[1])) or \
+                    ((y + 1 == ii[1] or y - 1 == ii[1]) and (x == ii[0])):
+                return True
+            else:
+                return False
+    else: return False
 
 
-def two_spaces_away(board: watchurback.BlankBoard, x, y):
-    for ii in board.index_white:
-        if ((x + 2 == ii[0] or x - 2 == ii[0]) and (y == ii[1])) or \
-                ((y + 2 == ii[1] or y - 2 == ii[1]) and (x == ii[0])):
-            return True
-        else:
-            return False
+def two_spaces_away(colour, board: watchurback.BlankBoard, x, y):
+    # returns whether a piece is 2 places away from x,y
+    if colour == WHITE:
+        for ii in board.index_white:
+            if ((x + 2 == ii[0] or x - 2 == ii[0]) and (y == ii[1])) or \
+                    ((y + 2 == ii[1] or y - 2 == ii[1]) and (x == ii[0])):
+                return True
+            else:
+                return False
+    elif colour == BLACK:
+        for ii in board.index_black:
+            if ((x + 2 == ii[0] or x - 2 == ii[0]) and (y == ii[1])) or \
+                    ((y + 2 == ii[1] or y - 2 == ii[1]) and (x == ii[0])):
+                return True
+            else:
+                return False
+    else: return False
