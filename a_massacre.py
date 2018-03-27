62dd789c31fc8c5e9a9ed2caa6be533f3704544d
RoryPowell <rpowell@student.unimelb.edu.au>
2018-03-27 16:49:32 +1100

    remove pdb lel

--- a_massacre.py
+++ a_massacre.py
@@ -5,6 +5,6 @@ def on_action(action: str, board: watchurback.BlankBoard):
     # TODO
     for ii in board.index_white:
         moves = board.get_valid_moves(ii)
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
 
     print('MASSACRE Not yet implemented.')
