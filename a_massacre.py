d69820e36681a1a97f50d28530d23bd4c4a47f2f
Kolatat Thangkasemvathana <kolatat.t@gmail.com>
2018-03-30 02:15:20 +1100

    More concise tree and no suicidal thoughts

--- a_massacre.py
+++ a_massacre.py
@@ -1,8 +1,9 @@
 import itertools
+import math
 import typing
 
 from spatial_utils import Coord, PAIRS, add_direction
-from watchurback import Board, BLACK, EMPTY, Piece, get_enemy
+from watchurback import Board, BLACK, EMPTY, Piece, get_enemy, CORNER
 
 
 class MassacreBoard(Board):
@@ -13,8 +14,9 @@ class MassacreBoard(Board):
         self._index_black = board._index_black
 
     def is_elimable(self, coord: Coord):
-        """ a coord is eliminatable if it is surrounded (axially) by EMPTY """
-        return self.is_surrounded(coord, EMPTY)
+        """ a coord is potentially eliminatable if it is surrounded (axially) by EMPTY, CORNER, or by enemy """
+        enemy = get_enemy(self.get_piece(coord))
+        return self.is_surrounded(coord, [EMPTY, enemy, CORNER])
 
     def get_elimable(self, player: Piece):
         return [coord for coord in self.index(player) if self.is_elimable(coord)]
@@ -22,14 +24,32 @@ class MassacreBoard(Board):
     def get_moves_til_elim_spec(self, target: Coord, killer1: Coord, killer2: Coord):
         """ gets (num moves, route for killer 1, route for killer 2) to kill target """
 
+        targetPiece = self.get_piece(target)
+        player = get_enemy(targetPiece)
+
         # assumes params are sane e.g. target is BLACK and killers are both WHITE cops
         def combined_flight_plan(dir_pair):
             pos1 = add_direction(target, dir_pair[0])
             pos2 = add_direction(target, dir_pair[1])
+            if not self.is_inside(pos1) or not self.is_inside(pos2):
+                return math.inf, []
+
+            pos1piece = self.get_piece(pos1)
+            pos2piece = self.get_piece(pos2)
+            if targetPiece in [pos1piece, pos2piece]:
+                # this pair aint gonna work
+                return math.inf, []
+
             # route and distance for each killer to get into position
-            (d1, r1) = self.get_min_dist(killer1, pos1, True)
-            # R2D2 would have been nicer but distance first is easier to sort
-            (d2, r2) = self.get_min_dist(killer2, pos2, True)
+            if pos1piece == CORNER:
+                (d1, r1) = (0, [])
+            else:
+                (d1, r1) = self.get_min_dist(killer1, pos1, True)
+            if pos2piece == CORNER:
+                # R2D2 would have been nicer but distance first is easier to sort
+                (d2, r2) = (0, [])
+            else:
+                (d2, r2) = self.get_min_dist(killer2, pos2, True)
             return d1 + d2, r1, r2
 
         return min([combined_flight_plan(dir_pair) for dir_pair in PAIRS])
@@ -43,12 +63,14 @@ class MassacreBoard(Board):
                     in itertools.permutations(enemies, 2)])
 
     def get_min_moves_til_elim(self, player: Piece):
-        return min([self.get_moves_til_elim_all(target) for target in self.index(player)])
+        return min([self.get_moves_til_elim_all(target) for target in self.get_elimable(player)])
 
     def execute_min_moves_til_elim(self, player: Piece):
         (dist, moves1, moves2) = self.get_min_moves_til_elim(player)
         # moves the furthest one first
-        avail_moves = [move for move in [moves1, moves2] if len(move) > 1]
+        # in calculating moves, we assumed that the destination could be of friendly units
+        # so we need to make sure move[1] is EMPTY to be a valid move
+        avail_moves = [move for move in [moves1, moves2] if len(move) > 1 and self.get_piece(move[1])==EMPTY]
         min_move = min(avail_moves, key=len)[:2]
         self.move(min_move[0], min_move[1])
         return min_move
@@ -60,6 +82,8 @@ def print_massacre_step(moves: typing.List[Coord]):
 
 def on_action(action: str, board: Board):
     m_board = MassacreBoard(board)
+    m_board.print_board()
     while not m_board.is_win():
         moves = m_board.execute_min_moves_til_elim(BLACK)
         print_massacre_step(moves)
+        m_board.print_board()
--- watchurback.py
+++ watchurback.py
@@ -58,19 +58,19 @@ class Board:
         # if len(line) != self.size:
         # WARNING incorrect size!
         for x in range(self._size):
-            self._set_cell((x, y), line[x])
+            self._set_cell(Coord(x, y), line[x])
 
     def _set_cell(self, coord: Coord, piece: Piece):
-        (x, y) = coord
-        if (x == 0 or x == self._size - 1) and (y == 0 or y == self._size - 1):
+        cornerRange = [0, self._size - 1]
+        if coord.x in cornerRange and coord.y in cornerRange:
             return
         # if piece not in [EMPTY, BLACK, WHITE]:
         # WARNING illegal piece
-        self._board[y][x] = piece
+        self._board[coord.y][coord.x] = piece
         if piece == WHITE:
-            self._index_white.append(Coord(x, y))
+            self._index_white.append(coord)
         elif piece == BLACK:
-            self._index_black.append(Coord(x, y))
+            self._index_black.append(coord)
         elif piece == EMPTY:
             # suppress ValueError because remove is noisy when index doesnt exist
             from contextlib import suppress
@@ -80,11 +80,7 @@ class Board:
                 self._index_black.remove(coord)
 
     def is_cell_valid(self, coord: Coord):
-        (x, y) = coord
-        # here (x,y) may be dum values
-        if x not in range(self._size) or y not in range(self._size):
-            return False
-        return self._board[y][x] == EMPTY
+        return self.is_inside(coord) and self.get_piece(coord) == EMPTY
 
     def get_valid_moves(self, coord: Coord):
         valid = []
@@ -107,6 +103,10 @@ class Board:
 
     def get_min_dist(self, coord_from: Coord, coord_to: Coord, dest_empty: bool = False):
         """return a tuple (distance, route) of the shortest route from to to"""
+        enemy = get_enemy(self.get_piece(coord_from))
+        original_to_piece = self.get_piece(coord_to)
+        if dest_empty:
+            self._set_cell(coord_to, EMPTY)
 
         # adapted from http://eddmann.com/posts/using-iterative-deepening-depth-first-search-in-python/
         def dfs(inner_route: List[Coord], inner_depth: int):
@@ -115,21 +115,30 @@ class Board:
             if inner_route[-1] == coord_to:
                 return inner_route
             valid_moves = self.get_valid_moves(inner_route[-1])
-            if dest_empty and inner_route[-1].is_adjacent(coord_to):
+            # if suicidal, not a valid move UNLESS its the final destination
+            if coord_to in valid_moves:
                 return inner_route + [coord_to]
+            # suicidal if surrounded by corner or enemy
+            valid_moves = [move for move in valid_moves if not self.is_surrounded(move, [CORNER, enemy])]
             for move in valid_moves:
                 if move not in inner_route:
                     next_route = dfs(inner_route + [move], inner_depth - 1)
                     if next_route:
                         return next_route
+            # blank returns (as in above as well) means its a dead end of dfs
+            return
 
         # it is possible that a route not exist so we limit to
         # 25 (just some arbitrary number)
+        default = (math.inf, [])
         for depth in range(25):
             route = dfs([coord_from], depth)
             if route:
-                return len(route) - 1, route
-        return math.inf, []
+                default = (len(route) - 1, route)
+                break
+        if dest_empty:
+            self._set_cell(coord_to, original_to_piece)
+        return default
 
     def get_piece(self, coord: Coord):
         return self._board[coord.y][coord.x]
@@ -154,18 +163,23 @@ class Board:
             self.elim_all(get_enemy(player))
             self.elim_all(player)
 
-    def check(self, coord: Coord, direction: Direction, for_piece: Piece):
+    def check(self, coord: Coord, direction: Direction, for_pieces: List[Piece]):
         """ :returns True if the piece to the $direction of $coord is a piece of type $for_piece """
-        return self.get_piece(add_direction(coord, direction)) == for_piece
+        rel_coord = add_direction(coord, direction)
+        if not self.is_inside(rel_coord):
+            # default behaviour for check outside boundary should be False cuz nothingness is not the same as anything
+            # not even EMPTY
+            return False
+        return self.get_piece(rel_coord) in for_pieces
 
     def can_elim(self, coord: Coord):
         player = self.get_piece(coord)
         if player not in [WHITE, BLACK]:
             # it breaks the law of physics to delete empty space without black holes
             return False
-        return self.is_surrounded(coord, get_enemy(player))
+        return self.is_surrounded(coord, [get_enemy(player), CORNER])
 
-    def is_surrounded(self, coord: Coord, by: Piece):
+    def is_surrounded(self, coord: Coord, by: List[Piece]):
         """ is coord surrounded by a type of piece """
         for dirs in PAIRS:
             if self.check(coord, dirs[0], by) and self.check(coord, dirs[1], by):
@@ -176,6 +190,9 @@ class Board:
         for piece in self.index(player):
             if self.can_elim(piece):
                 self._set_cell(piece, EMPTY)
+
+    def is_inside(self, coord: Coord):
+        return coord.x in range(self._size) and coord.y in range(self._size)
 
     def is_win(self, player: Piece = None):
         # u win if u have no enemy left
