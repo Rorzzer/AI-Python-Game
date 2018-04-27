3fcccfdecfb4871aad2f549a3810c846b7140493
RoryPowell <rpowell@student.unimelb.edu.au>
2018-04-27 17:17:44 +1000

    Begun Part B

---
+++ partb.py
@@ -0,0 +1,32 @@
+# Part B
+# File: partb.py
+#
+# TEAM The Dream Team
+
+import sys
+
+import a_massacre
+import a_moves
+from watchurback import Board
+
+WHITE = 'white'
+BLACK = 'black'
+
+
+class Player:
+
+    def __init__(self, colour):
+        if colour == WHITE:
+            self.colour = WHITE
+        elif colour == BLACK:
+            self.colour = BLACK
+        else:
+
+
+    def action(self, turns):
+
+
+
+    def update(self, action):
+
+
